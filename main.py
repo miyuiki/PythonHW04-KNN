da2530ddf86157719f2fcb0185f4fd74f5b34524
miyuiki <a121406@gmail.com>
2017-05-20 02:23:30 +0800

    Commit from Mac

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+# coding = utf8
 import codecs
+import sys
 if __name__ == '__main__':
-    input_fp = codecs.open('2015taiwan.txt','r',encoding = 'utf8')
-    output_fp = codecs.open('output.txt','w')
-    for line in input_fp:
-        line_list = line.split(',')
-        if line_list[2] != "PM2.5":
-            continue
-        else:
-            output_fp.write(str(line_list))
-            output_fp.write('\n')
+	input_fp = codecs.open('2015taiwan.txt', 'r')
+	output_fp = codecs.open('output.txt', 'w')
+	for line in input_fp:
+	line_list = line.split(',')
+		if line_list[2] != "PM2.5":
+			continue
+		else:
+			lin_list[1] = line_list[1].encode('big5')
+			output_fp.write(str(line_list).decode('big5'))
+			output_fp.write('\n')
+			print line_list

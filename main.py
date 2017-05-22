c9e94507a727490582109f428889553b865e7f71
Chang Jay <a121406@gmail.com>
2017-05-22 20:52:56 +0800

    pandas

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ from sklearn import cross_validation
 from sklearn.preprocessing import Imputer
 
 if __name__ == '__main__':
-    """pm_array = np.zeros((27705, 24), float)
-    pm_sum = []
-    for i in xrange(0,24):
-        new = []
-        pm_sum.append(new)
-    cnt = 0
     input_fp = open('2015taiwan.txt', 'r')
     buffer_fp = open('buffer.txt', 'w')
     fp = open('buffer.txt','r')
@@ -27,23 +21,20 @@ if __name__ == '__main__':
         else:
             for i in xrange(0,len(line_list)):
                 if i > 2 and not(line_list[i].isdigit()):
-                    line_list[i] = float('NaN')
-                if i < 2:
-                    continue
+                    line_list[i] = np.NaN
                 buffer_fp.write(str(line_list[i]) + " ")
         buffer_fp.write('\n')
-        for i in xrange(3,27):
-            pm_array[cnt][i-3] = line_list[i]
-        cnt += 1
-    for i in xrange(0,27705):
-        for j in xrange(0,24):
-            output.write(str(pm_array[i][j])+',')
-        output.write('\n')"""
-    fp = open("output.txt",'w')
-    data = pd.read_csv('2015taiwan.txt', sep=",", header=None)
+
+    data = pd.read_csv('buffer.txt', sep=" ", header=None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.max_rows', None)
     data.columns = ["date","location","obj","00","01","02","03","04","05","06","07","08","09",
-                    "10","11","12","13","14","15","16","17","18","19","20","21","22","23"]
-    print data[data.obj == 'PM2.5']
+                    "10","11","12","13","14","15","16","17","18","19","20","21","22","23","end"]
+    data.pop('end')
+    g = data.groupby(['location'])
+    output.write(str(data.fillna(g.mean())))
+    print g.mean()
+
 
 
 
